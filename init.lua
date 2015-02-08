80b325e492bccfac0bcf46c385d5b24bae032bb3
seniorivn <ivan.d.balashov@gmail.com>
2015-02-08 14:20:28 +0300

    v1.0 with only one of any  screen support(mousce.screen)

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
--- revelation .lua
+-- newtag .lua
+-- fork of revelation
+-- that:{
 --
 -- Library that implements Expose like behavior.
 --
@@ -8,7 +10,10 @@
 -- @auther Quan Guo guotsuan@gmail.com
 --
 -- @copyright 2008 Espen Wiborg, Julien Danjou
---
+--}
+--this is Library implements Expose like menu to choose clients for new tag and create it
+--with check for other tags for this combination of tags
+--@copyright 2015 Ivan Balashov ivan.d.balashov@gmail.com
 
 
 local beautiful    = require("beautiful")
@@ -29,15 +34,19 @@ local capi         = {
     screen         = screen
 }
 
-local revelation ={}
+local newtag ={}
 local clientData = {} -- table that holds the positions and sizes of floating clients
 
-charorder = "jkluiopyhnmfdsatgvcewqzx1234567890"
-hintbox = {} -- Table of letter wiboxes with characters as the keys
 
-revelation = {
+charorder = "htnsaoeuidjkbmpgclzvw" --"jkluiopyhnmfdsatgvcewqzx1234567890"
+charedit = "r"
+hintbox = {} -- Table of letter wiboxes with characters as the keys
+local markedclient = {}
+local count = 0
+
+newtag = {
     -- Name of expose tag.
-    tag_name = "Revelation",
+    tag_name = "NewTag",
 
     -- Match function can be defined by user.
     -- Must accept a `rule` and `client` and return `boolean`.
@@ -60,7 +69,8 @@ revelation = {
     },
     tags_status = {},
     is_excluded = false,
-    curr_tag_only = false
+    curr_tag_only = false,
+    screen = {capi.mouse.screen}
 }
 
 
@@ -87,7 +97,7 @@ end
 -- @param clients A table of clients to check.
 -- @param t The tag to give matching clients.
 local function match_clients(rule, clients, t, is_excluded)
-    local mfc = rule.any and revelation.match.any or revelation.match.exact
+    local mfc = rule.any and newtag.match.any or newtag.match.exact
     local mf = is_excluded and function(c,rule) return not mfc(c,rule) end or mfc 
     local k,v, flt
     for _, c in pairs(clients) do
@@ -104,7 +114,7 @@ local function match_clients(rule, clients, t, is_excluded)
 
             end
 
-            for k,v in pairs(revelation.property_to_watch) do
+            for k,v in pairs(newtag.property_to_watch) do
                 clientData[c][k] = c[k]
                 c[k] = v
                 
@@ -121,27 +131,29 @@ end
 -- @param rule A table with key and value to match. [{class=""}]
 
 
-function revelation.expose(args)
+function newtag.expose(args)
     local args = args or {}
     local rule = args.rule or {}
     local is_excluded = args.is_excluded or false
     local curr_tag_only = args.curr_tag_only or false
+    newtag.screen = args.screen or newtag.screen
 
-    revelation.is_excluded = is_excluded
-    revelation.curr_tag_only = curr_tag_only
+    newtag.is_excluded = is_excluded
+    newtag.curr_tag_only = curr_tag_only
 
     local t={}
     local zt={}
 
+    local clientlist = {}--awful.client.visible()
 
-    for scr=1,capi.screen.count() do
+    for i,scr in pairs(newtag.screen) do
 
         all_tags = awful.tag.gettags(scr)
 
-        t[scr] = awful.tag.new({revelation.tag_name},
+        t[scr] = awful.tag.new({newtag.tag_name},
         scr,
         awful.layout.suit.fair)[1]
-        zt[scr] = awful.tag.new({revelation.tag_name.."_zoom"},
+        zt[scr] = awful.tag.new({newtag.tag_name.."_zoom"},
         scr,
         awful.layout.suit.fair)[1]
 
@@ -153,11 +165,12 @@ function revelation.expose(args)
         end
 
         awful.tag.viewonly(t[scr], t.screen)
+	clientlist = awful.util.table.join(clientlist,awful.client.visible(scr))
     end 
 
 
     local hintindex = {} -- Table of visible clients with the hint letter as the keys
-    local clientlist = awful.client.visible()
+
     for i,thisclient in pairs(clientlist) do 
         -- Move wiboxes to center of visible windows and populate hintindex
         local char = charorder:sub(i,i)
@@ -170,21 +183,23 @@ function revelation.expose(args)
     end
 
     local function restore()
+	markedclient = {}
+	count = 0
         local k,v
-        for scr=1, capi.screen.count() do
+    	for i,scr in pairs(newtag.screen) do
             awful.tag.history.restore(scr)
             t[scr].screen = nil
         end
         capi.keygrabber.stop()
         capi.mousegrabber.stop()
-        for scr=1, capi.screen.count() do
+    	for i,scr in pairs(newtag.screen) do
             t[scr].activated = false
             zt[scr].activated = false
         end
 
         local clients
-        for scr=1, capi.screen.count() do
-            if revelation.curr_tag_only then 
+    	for i,scr in pairs(newtag.screen) do
+            if newtag.curr_tag_only then 
                 clients = awful.client.visible(scr)
             else
                 clients = capi.client.get(scr)
@@ -212,6 +227,75 @@ function revelation.expose(args)
         end
 
     end
+
+local function match (table1, table2)
+   for k, v in pairs(table1) do
+	   local bool = false
+	   for i,t in pairs(table2) do
+		   if v == t then
+			   bool = true
+		   end
+	   end
+	   if not bool then return false end
+   end
+   return true
+end
+
+    local function createresult(mark,co)
+	local markedclient = mark or {}
+	local count = co or 0
+	restore()
+
+	
+	local newtable = {}
+	if count < 2 then
+		--count = 0
+		return false
+	end
+	n = 1
+	for i,t in pairs(markedclient) do
+			newtable[n]=t
+			n = n + 1
+	end
+	--print(n.." "..#newtable)
+	--print(count)
+    	for i,scr in pairs(newtag.screen) do
+		for i,t in pairs(awful.tag.gettags(scr)) do
+			print(t.name)
+			clients = t.clients(t)
+			if (#clients == #newtable) then
+				if match(newtable,clients) then
+					awful.tag.viewonly(t)
+					--count = 0
+					return false
+				end
+			end
+		end
+	end
+	
+	local tagname = newtag.tag_name
+	local tag = awful.tag.add(tagname, { volatile = true, 
+				selected = true,
+				layout = awful.layout.suit.tile,
+				screen = capi.mouse.screen})
+
+	awful.tag.viewonly(tag)
+	
+
+	local name = ""
+	--markedclient = {}
+	--count = 0
+	
+	for i,j in pairs(markedclient) do
+		awful.client.toggletag(tag,j)
+            	j.maximized_horizontal = false --not c.maximized_horizontal
+            	j.maximized_vertical   = false --not c.maximized_vertical
+		name = name .. j.name:sub(0,2)
+	end
+	tag.name = name
+
+    end
+
 
     local zoomed = false
     local zoomedClient = nil
@@ -247,44 +331,64 @@ function revelation.expose(args)
             end
         end
 
-        if hintindex[key] then 
-            --client.focus = hintindex[key]
-            --hintindex[key]:raise()
-            
 
-            selectfn(restore)(hintindex[key])
 
-            for i,j in pairs(hintindex) do
-                hintbox[i].visible = false
-            end
+        	if hintindex[key] then 
+	    		if hintbox[key].visible then
+		    		markedclient[key] = hintindex[key]
+				count = count + 1
+	    		else
+		    		markedclient[key] = nil
+				count = count - 1
+	    		end
+	    		hintbox[key].visible = not hintbox[key].visible
+	            return false
+        	end 
 
-            return false
-        end 
-
-        if key == "Escape" then
-            for i,j in pairs(hintindex) do
-                hintbox[i].visible = false
-            end
-            restore()
-            return false
-        end
-
-        return true
+		--print(key)
+	
+	        if key == "Return" then
+	            for i,j in pairs(hintindex) do
+	                hintbox[i].visible = false
+	            end
+	            createresult(markedclient, count)
+	            return false
+	        end
+	        if key == "Escape" then
+	            for i,j in pairs(hintindex) do
+	                hintbox[i].visible = false
+	            end
+	            restore()
+	            return false
+	        end
+	
+	        return true
     end)
 
 
     local pressedMiddle = false
     local pressedRight = false
+    local pressedLeft = false
 
     capi.mousegrabber.run(function(mouse)
         local c = awful.mouse.client_under_pointer()
         if mouse.buttons[1] == true then
-            selectfn(restore)(c)
+	    if pressedLeft then
 
-            for i,j in pairs(hintindex) do
-                hintbox[i].visible = false
+            for i,cl in pairs(hintindex) do
+		    if (cl == c) then
+			if hintbox[i].visible then
+		    		markedclient[i] = hintindex[i]
+	    		else
+		    		markedclient[i] = nil
+	    		end
+	    		hintbox[i].visible = not hintbox[i].visible
+
+		    end
             end
-            return false
+	    end
+	    pressedLeft = not pressedLeft
+	    return true
         elseif mouse.buttons[2] == true and pressedMiddle == false and c ~= nil then 
             -- is true whenever the button is down. 
             pressedMiddle = true 
@@ -316,17 +420,17 @@ end
 
 -- Create the wiboxes, but don't show them
 
-function revelation.init(args)
+function newtag.init(args)
     hintsize = 60
     local fontcolor = beautiful.fg_normal
     local letterbox = {}
 
     local args = args or {}
 
-    revelation.tag_name = args.tag_name or revelation.tag_name
+    newtag.tag_name = args.tag_name or newtag.tag_name
     if args.match then 
-        revelation.match.exact = args.match.exact or revelation.match.exact
-        revelation.match.any = args.match.any or revelation.match.any
+        newtag.match.exact = args.match.exact or newtag.match.exact
+        newtag.match.any = args.match.any or newtag.match.any
     end
 
 
@@ -354,6 +458,6 @@ local function debuginfo( message )
 
     nid = naughty.notify({ text = tostring(mm), timeout = 10 })
 end
-setmetatable(revelation, { __call = function(_, ...) return revelation.expose(...) end })
+setmetatable(newtag, { __call = function(_, ...) return newtag.expose(...) end })
 
-return revelation
+return newtag
